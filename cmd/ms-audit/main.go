eb3e7b0e7af09f4fd408a5d4c43422f7d2b79d07
Håvard Elnan <havard.elnan@nhn.no>
2025-09-05 12:52:00 +0200

    use rorservice

--- cmd/ms-audit/main.go
+++ cmd/ms-audit/main.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-
 	"github.com/NorskHelsenett/ror-ms-audit/internal/auditconfig"
 	"github.com/NorskHelsenett/ror-ms-audit/internal/handlers/msauditrabbitmqhandler"
 
-	"syscall"
-
-	"github.com/NorskHelsenett/ror/pkg/config/configconsts"
-
-	"github.com/NorskHelsenett/ror/pkg/telemetry/trace"
-
-	healthserver "github.com/NorskHelsenett/ror/pkg/helpers/rorhealth/server"
-	"github.com/NorskHelsenett/ror/pkg/rlog"
-
-	"github.com/spf13/viper"
+	"github.com/NorskHelsenett/ror/pkg/services/rorservice"
 )
 
 func main() {
-	cancelChan := make(chan os.Signal, 1)
-	stop := make(chan struct{})
-	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT)
+	//TODO: mshelper
+	// config load interface
+	rorservice.MustInit()
 
-	rlog.Info("Audit micro service starting")
 	auditconfig.Load()
-
-	healthserver.MustStartWithDefaults()
-
-	trace.StartTracing(stop, cancelChan, viper.GetString(configconsts.ROLE), viper.GetString(configconsts.OPENTELEMETRY_COLLECTOR_ENDPOINT))
 
 	msauditrabbitmqhandler.StartListening()
 
-	sig := <-cancelChan
-	rlog.Info("Caught signal", rlog.Any("signal", sig))
+	rorservice.Wait()
 }
